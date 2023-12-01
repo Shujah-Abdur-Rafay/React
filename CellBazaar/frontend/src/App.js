97b8ba54df70d322fd5cf2957c50de15c1731865
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-01 11:37:38 +0500

    login credentials send to backend

--- CellBazaar/frontend/src/App.js
+++ CellBazaar/frontend/src/App.js
@@ -1,61 +1,60 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom";
-import Test from "./Test";
-
-export default function App() {
-  return (
-    <div>
-      <Test/>
-    </div>
-  )
-}
-
-
-
+ import 'react-bootstrap';
+ import 'bootstrap/dist/css/bootstrap.min.css'; 
+ import {Routes, Route} from "react-router-dom";
+ import Admin from './Components/Admin';
+ import {createContext} from "react";
+ import LandingPage from './Components/LandingPage';
+ import Product from './Components/CardS';
+ import Logins from './Components/Login';
+ import SignUp from './Components/Signup';
+ import AboutUs from './Components/AboutUs';
+ import Reviews from './Components/Reviews';
+ import FAQs from './Components/FAQs';
 
 
-
-// import 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import {Routes, Route} from "react-router-dom";
-// import Admin from './Components/Admin';
-// import {createContext} from "react";
-// import LandingPage from './Components/LandingPage';
-// import Product from './Components/CardS';
-// import Logins from './Components/Login';
-// import SignUp from './Components/Signup';
-// import AboutUs from './Components/AboutUs';
-// import Reviews from './Components/Reviews';
-// import FAQs from './Components/FAQs';
-
-
-// export const type=createContext();
-// function App() {
-//   return (
-//     <>
-//   <div>
+ export const type=createContext();
+ function App() {
+   return (
+     <>
+   <div>
 
    
-//     <Routes>
+     <Routes>
       
-// <Route path="/Admin" element={<Admin type="none"/>}/>
-// <Route path="" element={<LandingPage />}/>
-// <Route path="/about" element={<AboutUs />}/>
-// <Route path="/Reviews" element={<Reviews />}/>
-// <Route path="/Login" element={<Logins />}/>
-// <Route path="/FAQs" element={<FAQs />}/>
-// <Route path="/signup" element={<SignUp />}/>
-// <Route path="/add" element={<type.Provider value={"add"}><Admin/></type.Provider>}/>
-// <Route path="/delete" element={<type.Provider value={"delete"}><Admin/></type.Provider>}/>
-// <Route path="/update" element={<type.Provider value={"update"}><Admin/></type.Provider>}/>
-// <Route path="/view" element={<type.Provider value={"view"}><Admin/></type.Provider>}/>
-// <Route path="/Product" element={<Product/>}/>
+ <Route path="/Admin" element={<Admin type="none"/>}/>
+ <Route path="" element={<LandingPage />}/>
+ <Route path="/about" element={<AboutUs />}/>
+ <Route path="/Reviews" element={<Reviews />}/>
+ <Route path="/Login" element={<Logins />}/>
+ <Route path="/FAQs" element={<FAQs />}/>
+ <Route path="/signup" element={<SignUp />}/>
+ <Route path="/add" element={<type.Provider value={"add"}><Admin/></type.Provider>}/>
+ <Route path="/delete" element={<type.Provider value={"delete"}><Admin/></type.Provider>}/>
+ <Route path="/update" element={<type.Provider value={"update"}><Admin/></type.Provider>}/>
+ <Route path="/view" element={<type.Provider value={"view"}><Admin/></type.Provider>}/>
+ <Route path="/Product" element={<Product/>}/>
 
-//     </Routes> 
+     </Routes> 
   
-// </div>
-//     </>
-//   );
+ </div>
+     </>
+   );
+ }
+
+ export default App;
+
+
+
+
+// import React from 'react'
+// import {Routes, Route} from "react-router-dom";
+// import Test from "./Test";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Test/>
+//     </div>
+//   )
 // }
 
-// export default App;
