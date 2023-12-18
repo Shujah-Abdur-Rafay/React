bc0ba8c11b51677af7808d824d419081ed1dde19
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-18 16:08:09 +0500

    okk

--- CellBazaar/backend/index.js
+++ CellBazaar/backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
   {
-origin: ["https://cellbazaar-user.vercel.app/"], 
+origin: ["https://cellbazaar-user.vercel.app"], 
 methods: ["POST", "GET"],
   }
 ));
