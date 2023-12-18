496ca44e71e44b163ec6258a579660df4a4d68a3
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-18 10:45:20 +0500

    Revert "ADD,VIEW,DEL updated for vercel also api updated"
    This reverts commit ac301256e20a6ac1c2c2d361a14cc66978797baf.

--- CellBazaar/frontend/src/Components/Delete.js
+++ CellBazaar/frontend/src/Components/Delete.js
@@ -1,78 +1,75 @@
+
 import React, { useState } from 'react';
-import axios from 'axios';
+import { deleteViaAxios, getViaAxios } from '../Services/api';
 
 function Delete() {
-  const [view, setView] = useState({
-    id: "",
-  });
+ const [view,setView]=useState({
+  id:"",
+ });
 
-  const onChange = (e) => {
-    setView({ id: e.target.value });
-  };
+ const onChange=(e)=>{
+setView({id:e.target.value});
 
-  const onClick = async () => {
-    try {
-      // Set up Axios configuration
-      axios.defaults.withCredentials = true;
+ }
 
-      // Use Axios post instead of get, and send data in the request body
-      const res = await axios.post('https://deploy-mern-api.vercel.app/register', {
-        name: view.name,
-        email: view.email,
-        password: view.password
-      });
 
-      // Log the result
-      console.log(res);
 
-      // Rest of the code remains the same
-      const rep = res.data.filter((itm) => itm.productId === view.id);
 
-      if (rep.length === 0) {
-        alert("Product not found for the given ID");
-      } else {
-        alert("DELETED");
-        await axios.delete(`https://cellbazaar-api.vercel.app/api/products/${rep[0]._id}`);
-      }
-    } catch (error) {
-      console.error('Error deleting product:', error);
-      alert('Error deleting product. Please try again.');
+
+ const onClick = async () => {
+  try {
+    const res = await getViaAxios();
+    const rep = res.data.filter((itm) => itm.productId === view.id);
+
+    if (rep.length === 0) {
+      alert("Product not found for the given ID");
+    } else {
+      alert("DELETED");
+      await deleteViaAxios(rep[0]._id);
     }
-  };
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    alert('Error deleting product. Please try again.');
+  }
+};
+
+
 
   return (
     <div
-      style={{
-        marginLeft: '100px',
-        display: 'block',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        background: 'white',
-        padding: '40px',
-        borderRadius: '8px',
-        textAlign: 'center',
-      }}
+    style={{
+      marginLeft:'100px',
+      display: 'block',
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      background: 'white',
+      padding: '40px',
+      borderRadius: '8px',
+      textAlign: 'center',
+    }}
+  >
+    <h2>Confirm Deletion</h2>
+    <p style={{ fontSize: '18px' }}>Enter Model Number:</p>
+    <input
+      type="text"
+      onChange={onChange}
+      style={{ width: '243px', height: '40px', fontSize: '16px' }} 
+
+    />
+    
+    <br />
+    <button style={{ padding: '10px 20px', fontSize: '16px' }}>Cancel</button>
+    <button onClick={onClick}
+      style={{ backgroundColor: 'red', color: 'white', padding: '10px 20px', fontSize: '16px' }}
     >
-      <h2>Confirm Deletion</h2>
-      <p style={{ fontSize: '18px' }}>Enter Model Number:</p>
-      <input
-        type="text"
-        onChange={onChange}
-        style={{ width: '243px', height: '40px', fontSize: '16px' }}
-      />
+      Confirm Delete
+    </button>
+  </div>
 
-      <br />
-      <button style={{ padding: '10px 20px', fontSize: '16px' }}>Cancel</button>
-      <button
-        onClick={onClick}
-        style={{ backgroundColor: 'red', color: 'white', padding: '10px 20px', fontSize: '16px' }}
-      >
-        Confirm Delete
-      </button>
-    </div>
-  );
-}
+  
+);
+    }
 
-export default Delete;
+    export default Delete;
