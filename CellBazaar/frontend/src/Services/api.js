fb6272a89569ce5b4de6a0845ae46ff52687981a
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-18 10:50:53 +0500

    api updated

--- CellBazaar/frontend/src/Services/api.js
+++ CellBazaar/frontend/src/Services/api.js
@@ -1,33 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
-const url = "http://localhost:5000";
+axios.defaults.withCredentials=true
+
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export const sendMessageViaAxios = async (data) => {
     alert("ADDED");
-   await axios.post(`${url}/cellBazaar`,data);
+    await axios.post(`${apiUrl}/cellBazaar`, data);
 };
+
 export const getViaAxios = async () => {
-  return await axios.get(`${url}/cellBazaar`);
+    return await axios.get(`${apiUrl}/cellBazaar`);
 };
+
 export const deleteViaAxios = async (id) => {
-    return await axios.post(`${url}/cellBazaardel/:${id}`,id);
-  };
-
-  export const senduserViaAxios = async (userData) => {
-    alert("youre registered");
-   await axios.post(`${url}/cellBazaarsignup`,userData);
+    return await axios.post(`${apiUrl}/cellBazaardel/:${id}`, id);
 };
 
-
-
+export const sendUserViaAxios = async (userData) => {
+    alert("you're registered");
+    await axios.post(`${apiUrl}/cellBazaarsignup`, userData);
+};
 
 export const updateProductViaAxios = async (modelNumber, updatedData) => {
-  try {
-    const response = await axios.put(`${url}/cellBazaar/update/${modelNumber}`, updatedData);
-    alert("Product updated!");
-    return response.data;
-  } catch (error) {
-    console.error('Error updating product:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.put(`${apiUrl}/cellBazaar/update/${modelNumber}`, updatedData);
+        alert("Product updated!");
+        return response.data;
+    } catch (error) {
+        console.error('Error updating product:', error);
+        throw error;
+    }
 };
