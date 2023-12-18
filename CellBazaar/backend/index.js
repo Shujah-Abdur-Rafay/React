1586898254fa7abb733b568ca61cb83d48caa253
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-18 16:13:05 +0500

    change

--- CellBazaar/backend/index.js
+++ CellBazaar/backend/index.js
@@ -9,10 +9,7 @@ import SignUpModel from "./models/signupmodel.js";
 const app = express();
 
 app.use(cors(
-  {
-origin: ["https://cellbazaar-user.vercel.app"], 
-methods: ["POST", "GET"],
-  }
+ 
 ));
 app.use(express.json())
 
