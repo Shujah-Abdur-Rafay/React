c29daeb7949332b5d2be793d350ad06eb4bddb50
Shujah Abdur Rafay <145059961+Shujah-Abdur-Rafay@users.noreply.github.com>
2023-12-21 09:38:14 +0500

    Update product.js

--- CellBazaar/backend/product.js
+++ CellBazaar/backend/product.js
@@ -1,1 +1,19 @@
 
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+
+
+
+  productId: String,
+  image: String,
+  price: String,
+  name: String,
+  description: String,
+  lastUpdated: String,
+
+  
+});
+const productModel = mongoose.model("phone", productSchema);
+export default productModel;
+
