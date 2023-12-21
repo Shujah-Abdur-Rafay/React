1634256e09bc33fd810abde6f7f9d4f8109ed7c3
Shujah Abdur Rafay <145059961+Shujah-Abdur-Rafay@users.noreply.github.com>
2023-12-21 09:18:10 +0500

    Update index.js

--- CellBazaar/backend/index.js
+++ CellBazaar/backend/index.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import productModel from "./models/productmodel.js";
-import SignUpModel from "./models/signupmodel.js";
+// import productModel from "./models/productmodel.js";
+// import SignUpModel from "./models/signupmodel.js";
 
 
 const app = express();
@@ -39,75 +39,75 @@ app.listen(5000);
 app.use(bodyParser.json({extended : true}));
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.post("/cellBazaar",async (req , res)=>{
+// app.post("/cellBazaar",async (req , res)=>{
   
-     const post=new productModel({
-       productId: req.body.id,
-       name:req.body.name,
-       image:req.body.image,
-       price:req.body.price,
-       description:req.body.description,
-       lastUpdated:Date(),
-     });
+//      const post=new productModel({
+//        productId: req.body.id,
+//        name:req.body.name,
+//        image:req.body.image,
+//        price:req.body.price,
+//        description:req.body.description,
+//        lastUpdated:Date(),
+//      });
 
-console.log("Data Entered");
-await post.save();
-});
+// console.log("Data Entered");
+// await post.save();
+// });
 
-app.get("/cellBazaar",async (req , res)=>{
+// app.get("/cellBazaar",async (req , res)=>{
   
-const phones=await productModel.find();
-res.json(phones);
-console.log(phones);
-  });
+// const phones=await productModel.find();
+// res.json(phones);
+// console.log(phones);
+//   });
 
-       app.post("/cellBazaardel/:id",async (req , res)=>{
+      //  app.post("/cellBazaardel/:id",async (req , res)=>{
   
-         const id=req.params.id.slice(1);
-         await productModel.findByIdAndDelete(id);
-           });
+      //    const id=req.params.id.slice(1);
+      //    await productModel.findByIdAndDelete(id);
+      //      });
 
 
-      app.post("/cellBazaarsignup", async (req, res) => {
-        try {
-          const post = new SignUpModel({
-            username: req.body.username,
-            password: req.body.password,
-            phoneNo: req.body.phoneNo,
-          });
+      // app.post("/cellBazaarsignup", async (req, res) => {
+      //   try {
+      //     const post = new SignUpModel({
+      //       username: req.body.username,
+      //       password: req.body.password,
+      //       phoneNo: req.body.phoneNo,
+      //     });
       
-          await post.save();
-          res.json(post);
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Internal Server Error' });
-        }
-      });
+      //     await post.save();
+      //     res.json(post);
+      //   } catch (error) {
+      //     console.error(error);
+      //     res.status(500).json({ message: 'Internal Server Error' });
+      //   }
+      // });
 
 
       // ...
-      app.put("/cellBazaar/update/:id", async (req, res) => {
-        const productId = req.params.id;
+      // app.put("/cellBazaar/update/:id", async (req, res) => {
+      //   const productId = req.params.id;
       
-        try {
-          const updatedProduct = await productModel.findByIdAndUpdate(
-            productId,
-            { $set: req.body }, // Update with the data sent in the request body
-            { new: true, useFindAndModify: false }
-          );
+      //   try {
+      //     const updatedProduct = await productModel.findByIdAndUpdate(
+      //       productId,
+      //       { $set: req.body }, // Update with the data sent in the request body
+      //       { new: true, useFindAndModify: false }
+      //     );
       
-          if (updatedProduct) {
-            console.log('Product updated successfully:', updatedProduct);
-            res.json(updatedProduct);
-          } else {
-            console.log('Product not found');
-            res.status(404).json({ message: 'Product not found' });
-          }
-        } catch (error) {
-          console.error('Error updating product:', error);
-          res.status(500).json({ message: 'Internal Server Error' });
-        }
-      });
+      //     if (updatedProduct) {
+      //       console.log('Product updated successfully:', updatedProduct);
+      //       res.json(updatedProduct);
+      //     } else {
+      //       console.log('Product not found');
+      //       res.status(404).json({ message: 'Product not found' });
+      //     }
+      //   } catch (error) {
+      //     console.error('Error updating product:', error);
+      //     res.status(500).json({ message: 'Internal Server Error' });
+      //   }
+      // });
       
 // ...
 
