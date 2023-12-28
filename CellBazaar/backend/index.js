f8e9c6ef11014caf21ea5e0ae17a7a601d71557b
Shujah-Abdur-Rafay <shujaharafay7@gmail.com>
2023-12-28 17:40:43 +0500

    index imports

--- CellBazaar/backend/index.js
+++ CellBazaar/backend/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-// import productModel from "./models/productmodel.js";
+import productModel from "./models/productmodel.js";
 import SignUpModel from "./sign.js";
 import productModel from './product.js'
 import { GoogleOAuthProvider } from '@react-oauth/google';
